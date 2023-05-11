2b4b9985cf3f28ceb8addd4598a29abf2a54ea12
JohanCode2180100 <Johan_CODE21@orange.fr>
2023-05-11 23:46:23 +0200

    create password-genrator app and deleted component files for build indoor template

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<h1>Hello password</h1>`,
+  styles: [],
+})
+export class AppComponent {
+  title = 'password-generator';
+}
